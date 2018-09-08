158762ea77bf435343c30a856f34af5a4cfb748e
KristenLeach <31492991+KristenLeach@users.noreply.github.com>
2018-09-08 16:17:54 -0700

    Set up index.js file with thunk and provider store

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import rootReducer from './reducers/rootReducer'
+import thunk from 'redux-thunk'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById('root'));
 registerServiceWorker();
